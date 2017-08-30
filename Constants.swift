940376683d5d5e8f7fa8ae099e1990cc24ae5c21
dnylng <iamdannyluong@gmail.com>
2017-08-30 16:27:52 -0400

    Added cocoa pods
    Firebase, TMDB, SwiftyJSON, Alamofire, GoogleSignIn, and FBSignIn.

---
+++ Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  CineMap
+//
+//  Created by Danny Luong on 8/30/17.
+//  Copyright © 2017 dnylng. All rights reserved.
+//
+
+import Foundation
+
+// The API Key for TheMovieDB
+let TMDB_API_KEY = "5107c38310d2faa6938caedf92333fa0"
