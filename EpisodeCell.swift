7c69438104d9a1d28f54fe588c7bae5b03a30de4
dnylng <iamdannyluong@gmail.com>
2017-09-13 01:41:09 -0400

    Early setup for tv cell
    It loads 10 episodes with its episode counts. Each episode cell has its image, episode count, and up/down buttons.

---
+++ EpisodeCell.swift
@@ -0,0 +1,29 @@
+//
+//  EpisodeCell.swift
+//  CineMap
+//
+//  Created by Danny Luong on 9/13/17.
+//  Copyright © 2017 dnylng. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeCell: UICollectionViewCell {
+    
+    // MARK:- IBOUTLETS
+    
+    @IBOutlet weak var imageView: PosterImageView!
+    @IBOutlet weak var overlayHeight: NSLayoutConstraint!
+    @IBOutlet weak var episodeCount: UILabel!
+    
+    
+    // MARK:- EPISODE COUNT FUNCTIONS
+    
+    @IBAction func incrementCount(_ sender: Any) {
+        
+    }
+    
+    @IBAction func decrementCount(_ sender: Any) {
+        
+    }
+}
--- TVCell.swift
+++ TVCell.swift
@@ -8,14 +8,64 @@
 
 import UIKit
 
-class TVCell: UICollectionViewCell {
+class TVCell: UICollectionViewCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    // MARK:- IBOUTLETS
+    
+    @IBOutlet weak var currentlyWatchingBtn: UIButton!
+    @IBOutlet weak var planToWatchBtn: UIButton!
+    @IBOutlet weak var completedBtn: UIButton!
+    @IBOutlet weak var tvCollection: UICollectionView!
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK:- VARIABLES
+    
+    private var statusButtons: [UIButton]!
+    private var selectedStatus: Int!
+    
+    let cellId = "EpisodeCell"
+    
+    // MARK:- INITIALIZATION
+    
+    override func awakeFromNib() {
+        setupTVCollection()
+        setupStatusButtons()
     }
-    */
-
+    
+    fileprivate func setupTVCollection() {
+        tvCollection.dataSource = self
+        tvCollection.delegate = self
+        tvCollection.allowsSelection = true
+        
+        if let flowLayout = tvCollection.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.sectionInset = UIEdgeInsetsMake(0, 0, 10, 0)
+            flowLayout.minimumLineSpacing = 0
+            flowLayout.minimumInteritemSpacing = 0
+        }
+    }
+    
+    fileprivate func setupStatusButtons() {
+        statusButtons = [currentlyWatchingBtn, planToWatchBtn, completedBtn]
+        selectedStatus = statusButtons.index(of: currentlyWatchingBtn)
+        
+    }
+    
+    // MARK:- COLLECTION FUNCTIONS
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let ratio: CGFloat = 185/278
+        let width = collectionView.frame.width / 3
+        let height = width / ratio
+        let size = CGSize(width: width, height: height)
+        return size
+    }
+    
 }
